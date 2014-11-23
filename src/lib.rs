34b2f8b7366ff2a3d0e7f9ee26b7fdd9f3ce0ecd
Michael Yang <mikkyangg@gmail.com>
2014-11-23 16:50:31 -0500

    Add C pointer conversion trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 // Copyright 2014 Michael Yang. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+#![feature(macro_rules)]
 
+mod pointer;
 mod types;
 pub mod linear_equations;
