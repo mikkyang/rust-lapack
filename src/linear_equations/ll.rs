772155dc0e464e1e836c462218713b6fc5ff5dc5
Michael Yang <mikkyangg@gmail.com>
2014-11-23 16:32:40 -0500

    Add gesv bindings

--- src/linear_equations/ll.rs
+++ src/linear_equations/ll.rs
@@ -1,3 +1,37 @@
 // Copyright 2014 Michael Yang. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+
+extern crate libc;
+use self::libc::{
+    c_int,
+};
+
+use types::{
+    CLPK_complex,
+    CLPK_doublecomplex,
+    CLPK_doublereal,
+    CLPK_integer,
+    CLPK_real,
+};
+
+#[link(name = "lapack")]
+extern "C" {
+    pub fn sgesv_(n: *const CLPK_integer, nrhs: *const CLPK_integer, a: *mut CLPK_real,
+            lda: *const CLPK_integer, ipiv: *mut CLPK_integer, b: *mut CLPK_real,
+            ldb: *const CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn dgesv_(n: *const CLPK_integer, nrhs: *const CLPK_integer, a: *mut CLPK_doublereal,
+            lda: *const CLPK_integer, ipiv: *mut CLPK_integer, b: *mut CLPK_doublereal,
+            ldb: *const CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn cgesv_(n: *const CLPK_integer, nrhs: *const CLPK_integer, a: *mut CLPK_complex,
+            lda: *const CLPK_integer, ipiv: *mut CLPK_integer, b: *mut CLPK_complex,
+            ldb: *const CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn zgesv_(n: *const CLPK_integer, nrhs: *const CLPK_integer,
+            a: *mut CLPK_doublecomplex, lda: *const CLPK_integer,
+            ipiv: *mut CLPK_integer, b: *mut CLPK_doublecomplex,
+            ldb: *const CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+}
--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,23 @@
 // Copyright 2014 Michael Yang. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+
+extern crate libc;
+
+use self::libc::{
+    c_double,
+    c_float,
+    c_int,
+    c_void,
+};
+
+#[allow(non_camel_case_types)]
+pub type CLPK_integer = c_int;
+#[allow(non_camel_case_types)]
+pub type CLPK_real = c_float;
+#[allow(non_camel_case_types)]
+pub type CLPK_doublereal = c_double;
+#[allow(non_camel_case_types)]
+pub type CLPK_complex = c_void;
+#[allow(non_camel_case_types)]
+pub type CLPK_doublecomplex = c_void;
