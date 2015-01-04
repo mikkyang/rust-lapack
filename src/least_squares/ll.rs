d7e7adb770a6ae2731e87771e25cc858629e9770
Michael Yang <mikkyangg@gmail.com>
2015-01-04 13:44:53 +0800

    Add Gels trait

--- src/least_squares/ll.rs
+++ src/least_squares/ll.rs
@@ -16,4 +16,24 @@ use types::{
 
 #[link(name = "lapack")]
 extern "C" {
+    pub fn sgels_(trans: c_char, m: CLPK_integer, n: *const CLPK_integer,
+            nrhs: *const CLPK_integer, a: *mut CLPK_real, lda: *const CLPK_integer,
+            b: *mut CLPK_real, ldb: *const CLPK_integer, work: CLPK_real,
+            lwork: CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn dgels_(trans: c_char, m: CLPK_integer, n: *const CLPK_integer,
+            nrhs: *const CLPK_integer, a: *mut CLPK_doublereal, lda: *const CLPK_integer,
+            b: *mut CLPK_doublereal, ldb: *const CLPK_integer,
+            work: CLPK_doublereal, lwork: CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn cgels_(trans: c_char, m: CLPK_integer, n: *const CLPK_integer,
+            nrhs: *const CLPK_integer, a: *mut CLPK_complex, lda: *const CLPK_integer,
+            b: *mut CLPK_complex, ldb: *const CLPK_integer, work: CLPK_complex,
+            lwork: CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn zgels_(trans: c_char, m: CLPK_integer, n: *const CLPK_integer,
+            nrhs: *const CLPK_integer, a: *mut CLPK_doublecomplex,
+            lda: *const CLPK_integer, b: *mut CLPK_doublecomplex, ldb: *const CLPK_integer,
+            work: CLPK_doublecomplex, lwork: CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
 }
