5d2bbf7eb49721bf2c0be8684dcf814d7088b46f
Michael Yang <mikkyangg@gmail.com>
2015-01-04 14:11:06 +0800

    Add Ggev trait

--- src/general_eigenvalues/ll.rs
+++ src/general_eigenvalues/ll.rs
@@ -88,4 +88,35 @@ extern "C" {
             z: CLPK_doublecomplex, ldz: CLPK_integer,
             work: CLPK_doublecomplex, rwork: CLPK_doublereal,
             info: *mut CLPK_integer) -> c_int;
+
+    pub fn sggev_(jobvl: c_char, jobvr: c_char, n: *const CLPK_integer, a: *mut CLPK_real,
+            lda: *const CLPK_integer, b: *mut CLPK_real, ldb: *const CLPK_integer,
+            alphar: CLPK_real, alphai: CLPK_real, beta: *mut CLPK_real,
+            vl: CLPK_real, ldvl: CLPK_integer, vr: CLPK_real,
+            ldvr: CLPK_integer, work: CLPK_real, lwork: CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn dggev_(jobvl: c_char, jobvr: c_char, n: *const CLPK_integer,
+            a: *mut CLPK_doublereal, lda: *const CLPK_integer, b: *mut CLPK_doublereal,
+            ldb: *const CLPK_integer, alphar: CLPK_doublereal,
+            alphai: CLPK_doublereal, beta: *mut CLPK_doublereal,
+            vl: CLPK_doublereal, ldvl: CLPK_integer,
+            vr: CLPK_doublereal, ldvr: CLPK_integer,
+            work: CLPK_doublereal, lwork: CLPK_integer,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn cggev_(jobvl: c_char, jobvr: c_char, n: *const CLPK_integer,
+            a: *mut CLPK_complex, lda: *const CLPK_integer, b: *mut CLPK_complex,
+            ldb: *const CLPK_integer, alpha: *mut CLPK_complex, beta: *mut CLPK_complex,
+            vl: CLPK_complex, ldvl: CLPK_integer, vr: CLPK_complex,
+            ldvr: CLPK_integer, work: CLPK_complex, lwork: CLPK_integer,
+            rwork: CLPK_real,
+            info: *mut CLPK_integer) -> c_int;
+    pub fn zggev_(jobvl: c_char, jobvr: c_char, n: *const CLPK_integer,
+            a: *mut CLPK_doublecomplex, lda: *const CLPK_integer,
+            b: *mut CLPK_doublecomplex, ldb: *const CLPK_integer,
+            alpha: *mut CLPK_doublecomplex, beta: *mut CLPK_doublecomplex,
+            vl: CLPK_doublecomplex, ldvl: CLPK_integer,
+            vr: CLPK_doublecomplex, ldvr: CLPK_integer,
+            work: CLPK_doublecomplex, lwork: CLPK_integer,
+            rwork: CLPK_doublereal,
+            info: *mut CLPK_integer) -> c_int;
 }
