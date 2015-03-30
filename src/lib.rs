2979d5f046312aad40784e4c575497b6f897855e
Michael Yang <mikkyangg@gmail.com>
2015-03-30 18:02:07 -0400

    Remove stable feature gates

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 #![feature(concat_idents)]
-#![feature(globs)]
 #![feature(libc)]
-#![feature(macro_rules)]
 
 extern crate libc;
 extern crate num;
